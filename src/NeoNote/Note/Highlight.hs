4ab0bb15a8b8827361aaef39a4321f0530d7a4ae
Simre1 <simre4775@gmail.com>
2023-07-13 15:46:41 +0200

    Removed syntax highlighting due to issues with picker

--- src/NeoNote/Note/Highlight.hs
+++ src/NeoNote/Note/Highlight.hs
@@ -1,15 +1,12 @@
 module NeoNote.Note.Highlight where
 
 import Data.Coerce (coerce)
-import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Effectful
 import Effectful.Dispatch.Dynamic (interpret)
 import Effectful.TH (makeEffect)
 import NeoNote.Cache
 import NeoNote.Note.Note
-import Optics.Core
-import Skylighting
 
 data Highlight :: Effect where
   Highlight :: NoteInfo -> NoteContent -> Highlight m Text
@@ -18,7 +15,8 @@ makeEffect ''Highlight
 
 runHighlightWithCache :: (Cache :> es) => Eff (Highlight : es) a -> Eff es a
 runHighlightWithCache = interpret $ \_ (Highlight noteInfo noteContent) -> do
-  cache "highlight" noteInfo (pure $ highlightNoteContent noteInfo noteContent)
+  -- traceShowId <$> cache "highlight" noteInfo (pure $ highlightNoteContent noteInfo noteContent)
+  pure $ coerce noteContent
 
 runHighlight :: Eff (Highlight : es) a -> Eff es a
 runHighlight = interpret $ \_ (Highlight noteInfo noteContent) ->
@@ -26,17 +24,4 @@ runHighlight = interpret $ \_ (Highlight noteInfo noteContent) ->
     highlightNoteContent noteInfo noteContent
 
 highlightNoteContent :: NoteInfo -> NoteContent -> Text
-highlightNoteContent noteInfo noteContent = fromMaybe (coerce noteContent) $ do
-  syntax <- lookupSyntax (noteInfo ^. #extension) syntaxMap
-  either (const Nothing) Just $
-    formatANSI formatOptions style <$> source syntax
-  where
-    formatOptions = defaultFormatOpts
-    style = pygments
-    source syntax = tokenize tokenizerConfig syntax $ coerce noteContent
-    tokenizerConfig =
-      TokenizerConfig
-        { syntaxMap = syntaxMap,
-          traceOutput = False
-        }
-    syntaxMap = defaultSyntaxMap
+highlightNoteContent noteInfo noteContent = coerce noteContent
